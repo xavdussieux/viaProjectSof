7e7784fcfedd5072c39eff3c4617e65673dcc086
Xavier <xdussieux@gmail.com>
2016-08-03 21:30:50 +0200

    Add FPS feature

--- app/src/main/java/com/example/xavier/viaproject/GameLoopThread.java
+++ app/src/main/java/com/example/xavier/viaproject/GameLoopThread.java
@@ -8,6 +8,7 @@ import android.graphics.Canvas;
 public class GameLoopThread extends Thread {
     private GameView view;
     private boolean running = false;
+    static final long FPS = 50;
 
     public GameLoopThread(GameView view) {
         this.view = view;
@@ -19,11 +20,15 @@ public class GameLoopThread extends Thread {
 
     @Override
     public void run() {
+        long MSPT = 1000 / FPS; // milliseconds per tick
+        long startTime;
+        long sleepTime;
         int i = 0;
         while (running) {
             if(i % 20 == 0)
                 view.addNote();
             Canvas c = null;
+            startTime = System.currentTimeMillis();
             try {
                 c = view.getHolder().lockCanvas();
                 synchronized (view.getHolder()) {
@@ -35,6 +40,13 @@ public class GameLoopThread extends Thread {
                 }
             }
             i ++;
+            sleepTime = MSPT-(System.currentTimeMillis() - startTime);
+            try {
+                if (sleepTime > 0)
+                    sleep(sleepTime);
+                else
+                    sleep(10);
+            } catch (Exception e) {}
         }
     }
 }
