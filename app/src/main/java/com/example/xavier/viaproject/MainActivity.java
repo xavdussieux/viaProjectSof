f4728e9677d97f668aae411d6cecd27fbcd5418d
Xavier <xdussieux@gmail.com>
2016-08-04 10:12:01 +0200

    Add launch count feature

--- app/src/main/java/com/example/xavier/viaproject/MainActivity.java
+++ app/src/main/java/com/example/xavier/viaproject/MainActivity.java
@@ -14,6 +14,14 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Logger;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +41,38 @@ public class MainActivity extends AppCompatActivity {
                 System.exit(0);
             }
         });
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        database.setLogLevel(Logger.Level.DEBUG);
+        final DatabaseReference appLaunchCount = database.getReference("appLaunchCount");
+
+        appLaunchCount.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Fetch the value from the snapshot of the data, as an Integer
+                Integer value = dataSnapshot.getValue(Integer.class);
+
+                // If the value is null, then the data wasn't defined in the database, so default to 1.
+                int newValue;
+                if (value == null) {
+                    newValue = 1;
+                } else {
+                    newValue = value + 1;
+                }
+
+                // Upload the new value to the database
+                appLaunchCount.setValue(newValue);
+                // Toast the amount of times the app has been opened
+                Toast.makeText(MainActivity.this, "App launched: " + newValue + " times", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle errors here
+            }
+        });
+
+
     }
 
     @Override
