54c9bbb7dc7299a99d6f47065e45c82c23f0da4b
Xavier <xdussieux@gmail.com>
2016-08-10 01:06:19 +0200

    Note class cleaned

--- app/src/main/java/com/example/xavier/viaproject/Note.java
+++ app/src/main/java/com/example/xavier/viaproject/Note.java
@@ -1,17 +1,10 @@
 package com.example.xavier.viaproject;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
-import android.preference.PreferenceManager;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.TypedValue;
 
 import java.util.ArrayList;
@@ -22,12 +15,6 @@ import java.util.List;
  */
 public class Note {
 
-    private Bitmap mBlue;
-    private Bitmap mGreen;
-    private Bitmap mYellow;
-    private Bitmap mRed;
-    private int mEndY;
-    private int mNoteSize;
     private int mScreeny;
     private int mBlueX;
     private int mRedX;
@@ -35,7 +22,7 @@ public class Note {
     private int mYellowX;
     private Score mScore;
     private float mSpeed;
-    private float radius;
+    private float mRadius;
     private GameLoopThread mGameLoopThread;
 
     public class TypeNote {
@@ -55,28 +42,17 @@ public class Note {
 
         //resizing notes according to the player's screen
         mScreeny = screenY;
-        mNoteSize = screenX / 5;
-        mGreenX = screenX / 10 + fromDP(context,20);
-        mRedX = screenX * 3 / 10 + fromDP(context,32);
-        mYellowX = screenX * 5 / 10 + fromDP(context,45);
-        mBlueX = screenX * 7 / 10 + fromDP(context,58);
-        mEndY = screenY - 3 * mNoteSize / 2;
-        float endY = screenY - 3 * mNoteSize / 2;
+        int noteSize = screenX / 6;
+        mGreenX = screenX * 55 / 355;// + fromDP(context,20);
+        mRedX = screenX * 139 / 355;// + fromDP(context,32);
+        mYellowX = screenX * 224 / 355;// + fromDP(context,45);
+        mBlueX = screenX * 305 / 355;// + fromDP(context,58);
+        float endY = screenY - 3 * noteSize / 2;
         mScore = score;
         float interval = endY - Constants.NOTE_START_Y;
-        radius = mScreeny / 18;
-        interval = mEndY - Constants.NOTE_START_Y;
+        mRadius = mScreeny / 18;
 
         mSpeed = interval / scrolling_time;
-
-        mBlue = BitmapFactory.decodeResource(context.getResources(), R.drawable.bluenote);
-        mGreen = BitmapFactory.decodeResource(context.getResources(), R.drawable.greennote);
-        mRed = BitmapFactory.decodeResource(context.getResources(), R.drawable.rednote);
-        mYellow = BitmapFactory.decodeResource(context.getResources(), R.drawable.yellownote);
-        mBlue = Bitmap.createScaledBitmap(mBlue, mNoteSize, mNoteSize, false);
-        mGreen = Bitmap.createScaledBitmap(mGreen, mNoteSize, mNoteSize, false);
-        mRed = Bitmap.createScaledBitmap(mRed, mNoteSize, mNoteSize, false);
-        mYellow = Bitmap.createScaledBitmap(mYellow, mNoteSize, mNoteSize, false);
 
         mNotes = new ArrayList<TypeNote>();
         mNotesToRemove = new ArrayList<TypeNote>();
@@ -88,16 +64,16 @@ public class Note {
         paint.setColor(Color.argb(255, 249, 129, 0));
         switch (noteType) {
             case "green":
-                mNotes.add(new TypeNote(mGameLoopThread.getTime(), new Point(mGreenX, -200)));
+                mNotes.add(new TypeNote(mGameLoopThread.getTime(), new Point(mGreenX, Constants.NOTE_START_Y)));
                 break;
             case "red":
-                mNotes.add(new TypeNote(mGameLoopThread.getTime(), new Point(mRedX, -200)));
+                mNotes.add(new TypeNote(mGameLoopThread.getTime(), new Point(mRedX, Constants.NOTE_START_Y)));
                 break;
             case "yellow":
-                mNotes.add(new TypeNote(mGameLoopThread.getTime(), new Point(mYellowX, -200)));
+                mNotes.add(new TypeNote(mGameLoopThread.getTime(), new Point(mYellowX, Constants.NOTE_START_Y)));
                 break;
             case "blue":
-                mNotes.add(new TypeNote(mGameLoopThread.getTime(), new Point(mBlueX, -200)));
+                mNotes.add(new TypeNote(mGameLoopThread.getTime(), new Point(mBlueX, Constants.NOTE_START_Y)));
                 break;
         }
     }
@@ -122,11 +98,11 @@ public class Note {
 
             //switch needs constants
             if (note.pos.x == mGreenX)
-                canvas.drawCircle(note.pos.x, note.pos.y, radius, paintGreen);
-            if (note.pos.x == mRedX) canvas.drawCircle(note.pos.x, note.pos.y, radius, paintRed);
+                canvas.drawCircle(note.pos.x, note.pos.y, mRadius, paintGreen);
+            if (note.pos.x == mRedX) canvas.drawCircle(note.pos.x, note.pos.y, mRadius, paintRed);
             if (note.pos.x == mYellowX)
-                canvas.drawCircle(note.pos.x, note.pos.y, radius, paintYellow);
-            if (note.pos.x == mBlueX) canvas.drawCircle(note.pos.x, note.pos.y, radius, paintBlue);
+                canvas.drawCircle(note.pos.x, note.pos.y, mRadius, paintYellow);
+            if (note.pos.x == mBlueX) canvas.drawCircle(note.pos.x, note.pos.y, mRadius, paintBlue);
             int dt = mGameLoopThread.getTime() - note.spawnTime;
             note.pos.y = (int) (dt * mSpeed + Constants.NOTE_START_Y);
             if (note.pos.y > mScreeny) {
@@ -153,24 +129,15 @@ public class Note {
 
     public boolean onNoteWithDel(Point touchedPoint) {
         int dx, dy;
-        TypeNote del = null;
         for (TypeNote note : mNotes) {
             dx = touchedPoint.x - note.pos.x;
             dy = touchedPoint.y - note.pos.y;
-            if ((Math.sqrt(dx * dx + dy * dy) < radius))
+            if ((Math.sqrt(dx * dx + dy * dy) < mRadius))
             {
                 addNoteToRemove(note);
                 return true;
             }
         }
         return false;
-    }
-
-    public int fromDP(Context context, float dp) {
-        return (int) TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                dp,
-                context.getResources().getDisplayMetrics()
-        );
     }
 }
